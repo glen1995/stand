80212d85cad9f649d2424140ffa19b4edc456cb6
glenthomas <glenkt2@gmail.com>
2024-10-06 21:46:19 -0700

    init commit

---
+++ src/background.ts
@@ -0,0 +1,8 @@
+let active = false;
+
+function makeOrange(color: string): void {
+    document.body.style.backgroundColor = color;
+}
+
+console.log("hii2");
+
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: {
+        background: path.resolve(__dirname, "src", "background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: ".", context: "public" }, {
+                from: "./manifest.json", to: "./manifest.json"
+            }]
+        }),
+    ],
+    devServer: {
+        compress: true,
+        port: 9000,
+        hot: true,
+    },
+};
