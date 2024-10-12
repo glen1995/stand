b4dce2f5dad5bbc67c572c8eb5243b2e16501774
glenthomas <glenkt2@gmail.com>
2024-10-12 16:14:23 -0700

    chore: remove node_modules and dist from commit

--- dist/background.js
+++ dist/background.js
@@ -8,26 +8,7 @@
  */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/background.ts":
-/*!***************************!*\
-  !*** ./src/background.ts ***!
-  \***************************/
-/***/ (() => {
-
-eval("\nlet active = false;\nfunction makeOrange(color) {\n    document.body.style.backgroundColor = color;\n}\nconsole.log(\"hii2\");\n\n\n//# sourceURL=webpack://reminder-tab/./src/background.ts?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/background.ts"]();
 /******/ 	
 /******/ })()
 ;
