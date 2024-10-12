3bf657532fb48417678c9bd996a18be1bbcd68c6
glenthomas <glenkt2@gmail.com>
2024-10-12 16:54:47 -0700

    chore: adding types to localstorage

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -2,17 +2,14 @@ export type TimerConfig = {
     time: number;
 }
 
-export class Configs {
-    timerConfig: TimerConfig = { time: 15 };
-}
 
 export const localStorage = {
-    get: (callback: (configs: TimerConfig) => void) => {
-        chrome.storage.sync.get({ configs: new Configs() }, (storage) => {
+    get: (callback: (configs: TimerConfig) => void, key: keyof TimerConfig) => {
+        chrome.storage.sync.get(key, (storage) => {
             callback(storage.configs);
         });
     },
-    set: (configs: Configs, callback?: () => void) => {
+    set: (configs: TimerConfig, callback?: () => void) => {
         chrome.storage.sync.set({ configs }, () => {
             callback?.();
         });
