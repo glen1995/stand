d1f568a7c8662471b25a90fa36382b86e56c016f
glenthomas <glenkt2@gmail.com>
2024-10-21 23:47:09 -0700

    chroe: clean up old code

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -4,8 +4,6 @@ export type TimerConfig = {
     time: number;
 }
 
-
-
 export function getStorageData(): Promise<TimerConfig> {
     return new Promise((resolve, reject) => {
         chrome.storage.sync.get(null, (result) => {
@@ -29,22 +27,3 @@ export function setStorageData(data: TimerConfig): Promise<void> {
         });
     });
 }
-// export const localStorage = {
-//     get: (callback: (configs: Record<string, string>) => void, key: keyof TimerConfig) => {
-//         chrome.storage.sync.get(key, (storage) => {
-//             callback(storage);
-//         });
-//     },
-//     set: (configs: TimerConfig, callback?: () => void) => {
-//         chrome.storage.sync.set(configs, () => {
-//             callback?.();
-//         });
-//     },
-//     listen: (callback: (configs: TimerConfig) => void) => {
-//         chrome.storage.onChanged.addListener((changes) => {
-//             if (changes.configs) {
-//                 callback(changes.configs.newValue);
-//             }
-//         });
-//     },
-// };
